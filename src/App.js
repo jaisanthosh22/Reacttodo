eca5cfbb3b3cc2e858a5af54bb0eac108ce6b021
jai santhosh <jai2201santhosh@gmail.com>
2025-02-09 08:03:36 +0530

    image placeholder template is created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Website  developer</h1>
+    <img src="https://placehold.co/400" alt="Placeholder" />
+    <img style={{marginLeft: '10px'}} src="https://placehold.co/600x400" alt="Placeholder" />
+    <img style={{marginLeft: '10px'}} src="https://placehold.co/600x400" alt="Placeholder" />
+  </div>
+  );
+}
+
+export default App;
