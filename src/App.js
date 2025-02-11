6531f1111570f47650aa8e52445f03a93bbaf24f
jai santhosh <jai2201santhosh@gmail.com>
2025-02-11 16:48:03 +0530

    destructuring props

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ function App() {
     <AppUserComponent name="Jai santhosh R" proffession="software developer"/>
     <AppUserComponent name="Anand nagaraj R" proffession="Bsc Agriculture" />
     <AppUserComponent name="Rajarajan C" proffession="Advocate"/>
-    <AppUserComponent name="Meenakshi K" proffession="teacher"/>
+    <AppUserComponent name="Meenakshi" proffession="teacher"/>
+    <AppUserComponent name="Balasubramanian" proffession="sap manager"/>
+    <AppUserComponent name="Kumaresan" proffession="manager" url={"https://placehold.co/100/orange/white"}/>
+    
+
 
     
     
