a86a0f1db3fd916c55f17a89cfe4cefdfbc4b5da
jai santhosh <jai2201santhosh@gmail.com>
2025-02-12 07:03:09 +0530

    creating list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import AppUserComponent from './components/AppUserComponent';
+import { AppUserList } from './components/AppUserList';
+
 
 
 
@@ -8,20 +9,8 @@ import AppUserComponent from './components/AppUserComponent';
 function App() {
   return (
     <div className='App'>
-    <AppUserComponent name="Jai santhosh R" proffession="software developer"/>
-    <AppUserComponent name="Anand nagaraj R" proffession="Bsc Agriculture" />
-    <AppUserComponent name="Rajarajan C" proffession="Advocate"/>
-    <AppUserComponent name="Meenakshi" proffession="teacher"/>
-    <AppUserComponent name="Balasubramanian" proffession="sap manager"/>
-    <AppUserComponent name="Kumaresan" proffession="manager" url={"https://placehold.co/100/orange/white"}/>
-    
-
-
-    
-    
-    
-
-    </div>
+    <AppUserList/>
+   </div>
    );
 }
 
